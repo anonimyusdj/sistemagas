fb20299b7e485135782086952cf4871a8272732f
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-11 14:46:40 -0600

    Update model.database.php
    conexion

---
+++ model/model.database.php
@@ -0,0 +1,14 @@
+<?php
+  class Database{
+    public static function conectar(){
+          $host="35.231.175.105";
+          $dbname="gasolinerapuma";
+          $charset="utf8";
+          $user="root";
+          $pass="2016.Deisy";
+      $pdo=new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=".$charset,$user,$pass);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    }
+  }
+?>
