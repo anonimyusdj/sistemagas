c340264da1cc8e95fd4a43a3d00ff15630efef7a
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-13 18:20:11 -0600

    Update cargos.model.php
    get cargos by id

--- model/cargos.model.php
+++ model/cargos.model.php
@@ -41,6 +41,28 @@
       }
 
     }
+    public function getcargosid($id){
+      try {
+          $sql="SELECT * FROM cargos where id_cargo=?";
+          $stm= $this->conexion->prepare($sql);
+          $stm->execute(array($id));
+          $salida="";
+        
+          while($rs=$stm->fetch(PDO::FETCH_ASSOC)){
+             if($salida != ""){
+              $salida .= ",";
+            }
+            $salida .= '{"codigo":"'.$rs['id_cargo'].'","nombre":"'.$rs['nombre_cargo'].'","nivel":"'.$rs['nivel'].'","fecha":"'.$rs['fecha_creacion'].'"}';
+          }
+          $salida='{"resultado":['.$salida.']}';
+          echo ($salida);
+            
+     
+      } catch (Exception $e) {
+        echo "{'resultado':{'".$e->getMessage()."'}}";
+      }
+
+    }
 
   }
   /*$cargo=new CargosModel();
