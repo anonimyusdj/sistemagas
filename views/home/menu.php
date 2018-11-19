76c0d1adf35646bbda42a7c1003e96fb22b2a181
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-18 18:04:03 -0600

    menu home

---
+++ views/home/menu.php
@@ -0,0 +1,10 @@
+ <nav>
+      <div class="nav-wrapper teal darken-4">
+        <a href="#" class="brand-logo"><img src="assets\multimedia\imagenes\pumalogo.png" style="width:250px; height:50px;" class="responisve-img"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
