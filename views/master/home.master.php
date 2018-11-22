813d6f0533e085b578d31f4bd6a77abc973909c2
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-22 16:57:25 -0600

    error

--- views/master/home.master.php
+++ views/master/home.master.php
@@ -3,7 +3,7 @@
   <div class="col s12 m6 card">
     <form>
       <div class="row"
-       <div class="input-fiel col s12 m4">
+       <div class="input-field col s12 m4">
         <input id="preCompra" type="numbrer" class="validate" ng-model="preCompra">
         <label for="preCompra">Precio de Compra</label>
       </div>
