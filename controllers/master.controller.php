a91bd17d3400008b8f44c73bec07db4b18a99ca0
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-22 13:43:39 -0600

    home master

--- controllers/master.controller.php
+++ controllers/master.controller.php
@@ -6,7 +6,7 @@
     public function home(){
       require_once "views/partes/header.php";
       //require_once "views/home/menu.php";
-      require_once "views/master/home.php";
+      require_once "views/master/home.master.php";
       require_once "views/partes/footer.php";
     }
   }
