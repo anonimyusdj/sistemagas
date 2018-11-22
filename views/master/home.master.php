be746490639cf0fea9b092ffc5dc16b08d4e16f7
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-22 16:56:38 -0600

    formulario factura

--- views/master/home.master.php
+++ views/master/home.master.php
@@ -1,1 +1,13 @@
-<h1>Hola Master</h1>
+<div class="row">
+  <div class="col s12 m2"></div>
+  <div class="col s12 m6 card">
+    <form>
+      <div class="row"
+       <div class="input-fiel col s12 m4">
+        <input id="preCompra" type="numbrer" class="validate" ng-model="preCompra">
+        <label for="preCompra">Precio de Compra</label>
+      </div>
+      </div>
+    </form>
+  </div>
+</div>
