6e9ab9dbcbba700252290be0c6481b915f18aa50
Edgar Guzman <anonimyusdj@gmail.com>
2018-12-06 21:13:50 -0600

    vista master

--- controllers/master.controller.php
+++ controllers/master.controller.php
@@ -6,8 +6,8 @@
     public function home(){
       require_once "views/partes/header.php";
       //require_once "views/home/menu.php";
-     // require_once "views/master/home.master.php";
-      require_once "views/empleados/new_empleado.php";
+     require_once "views/master/home.master.php";
+     // require_once "views/empleados/new_empleado.php";
       require_once "views/partes/footer.php";
     }
   }
